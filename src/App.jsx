f00dbbb4bd182ff79955e3b34b9fa0c836a2e8e7
freptry <freptry101@gmail.com>
2024-10-15 10:59:17 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/graphyprintpro" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="branding" element={<Branding />} />
           <Route path="forprint" element={<Forprint />} />
